e345a98f01f08063c3cf5c67cf3866cc02b3456c
Zachary Morris <70924115+Zacham17@users.noreply.github.com>
2023-12-10 20:39:10 -0500

    Create file_decrypt.py

---
+++ SEC440/project4/file_decrypt.py
@@ -0,0 +1,27 @@
+from cryptography.hazmat.backends import default_backend
+from cryptography.hazmat.primitives.ciphers import Cipher, algorithms, modes
+import os
+
+def decrypt_file(file_path, symmetric_key):
+    with open(file_path, 'rb') as file:
+        ciphertext = file.read()
+
+    cipher = Cipher(algorithms.AES(symmetric_key), modes.CFB(b'\0' * 16), backend=default_backend())
+    decryptor = cipher.decryptor()
+    plaintext = decryptor.update(ciphertext) + decryptor.finalize()
+
+    with open(file_path[:-4], 'wb') as decrypted_file:
+        decrypted_file.write(plaintext)
+
+def main():
+    # Read the decrypted symmetric key from sym_key.dec
+    with open('sym_key.dec', 'rb') as sym_key_file:
+        symmetric_key = sym_key_file.read()
+
+    # Decrypt all ".zam" files
+    for filename in os.listdir('.'):
+        if filename.endswith('.getRekt'):
+            decrypt_file(filename, symmetric_key)
+
+if __name__ == "__main__":
+    main()
